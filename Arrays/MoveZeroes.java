91e6c741be035f61f5b5fce3045b90648677946e
mansirathor27 <mansirathor575@gmail.com>
2025-06-18 23:28:54 +0530

    Added solution for moving all the zeroes at the end of array

---
+++ Arrays/MoveZeroes.java
@@ -0,0 +1,26 @@
+package Arrays;
+public class MoveZeroes {
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[lastNonZeroFoundAt];
+                nums[lastNonZeroFoundAt] = nums[i];
+                nums[i] = temp;
+                lastNonZeroFoundAt++;
+            }
+        }
+        // for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
+        //     nums[i] = 0;
+        // }
+    }
+}
